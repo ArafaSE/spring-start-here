e65c7b853c8dd59c513f88c73c66034c45ec419c
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-04-25 12:05:30 +0200

    Using Path variables to send data from client to server

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -2,21 +2,18 @@ package main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
-    /*
-     We define a new parameter for the controller’s action method and annotate it with @RequestParam.
-     We also add the Model parameter that we use to send data from the controller to the view.
-     */
+    // To define a path variable, you assign it a name and put it in the path between curly braces.
+    @RequestMapping("/{color}")
     public String home(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String color,
+            @PathVariable String color,
             Model page) {
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Katy");
         page.addAttribute("color", color);
         return "home.html";
     }
