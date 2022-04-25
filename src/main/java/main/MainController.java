70d8a82b43a38df7044a12e8c5d083de9caa1e32
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-04-25 11:39:40 +0200

    Implement Dynamic template

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -1,13 +1,19 @@
 package main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
+ The @Controller stereotype annotation marks this class as Spring MVC controller
+ and adds a bean of this type to the Spring context.
+ */
 @Controller
 public class MainController {
-
     @RequestMapping("/")
-    public String home(){
+    // The action method defines a parameter of type Model that stores the data the controller sends to the view.
+    public String home(Model page){
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
         return "home.html";
     }
 }
