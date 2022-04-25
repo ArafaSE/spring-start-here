3c071b604fbb8b6cbd78c5a7d942bae1add6be02
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-04-25 11:55:24 +0200

    Using request params to send data from client to server

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -3,17 +3,21 @@ package main;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-/*
- The @Controller stereotype annotation marks this class as Spring MVC controller
- and adds a bean of this type to the Spring context.
- */
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class MainController {
     @RequestMapping("/")
-    // The action method defines a parameter of type Model that stores the data the controller sends to the view.
-    public String home(Model page){
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+    /*
+     We define a new parameter for the controller’s action method and annotate it with @RequestParam.
+     We also add the Model parameter that we use to send data from the controller to the view.
+     */
+    public String home(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String color,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
