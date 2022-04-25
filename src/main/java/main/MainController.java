602f0f81d5c9388e2682307f5f119fa93b99af59
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-04-25 13:59:37 +0200

    Implement GET and POST Requests

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -2,18 +2,18 @@ package main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    // To define a path variable, you assign it a name and put it in the path between curly braces.
-    @RequestMapping("/{color}")
+    @GetMapping("/home")
     public String home(
-            @PathVariable String color,
+            @RequestParam String name,
+            @RequestParam String color,
             Model page) {
-        page.addAttribute("username", "Katy");
+        page.addAttribute("name", name);
         page.addAttribute("color", color);
         return "home.html";
     }
