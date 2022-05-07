34475937c192f3c03e72b27437d16065f0b42bf7
Mohamed Arafa <mohamed@1-Tech-MohamedArafa.local>
2022-05-07 11:55:12 +0200

    Implement Country DTO

---
+++ src/main/java/controllers/CountryController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public Country france() {
+        return Country.of("France", 67);
+    }
+
+    @GetMapping("/all")
+    public List<Country> countries() {
+        Country c1 = Country.of("France", 67);
+        Country c2 = Country.of("Spain", 48);
+
+        return List.of(c1, c2);
+    }
+}
---
+++ src/main/java/model/Country.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Country {
+    private String name;
+    private int population;
+
+    // Our DTO Object model (data transfer object)
+    public static Country of(
+            String name,
+            int population
+    ){
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
